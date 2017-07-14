e7cf25ee99b4468efea581346db30b96895ce1e7
Craig Copeland <copelandhouse2@yahoo.com>
2017-07-13 21:27:25 -0500

    Rock Paper Scissors game

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -7,12 +7,51 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
+// Ah Rock Paper scissors
+// Ties: rock:rock; paper:paper; scissors:scissors
+// Hand1 wins: rock:scissors; paper:rock; scissors:paper
+// Hand2 wins: scissors:rock; rock:paper; paper:scissors
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+  hand1 = hand1.toLowerCase().trim();
+  hand2 = hand2.toLowerCase().trim();
 
-}
+  let winningMessage = '';
+
+  if ( (hand1 !== 'rock' && hand1 !== 'paper' && hand1 !== 'scissors') ||
+       (hand2 !== 'rock' && hand2 !== 'paper' && hand2 !== 'scissors')
+     ) {
+    winningMessage = "Alright, someone did not enter a valid value";
+    return winningMessage;
+  }
+
+  if (hand1 === hand2) {
+    winningMessage = "It's a tie!"
+  }
+  else if (hand1 === 'rock') {
+    if (hand2 === 'paper') {
+      winningMessage = "Hand two wins!";
+    } else {
+      winningMessage = "Hand one wins!";
+    }
+  } // else if hand 1 = rock
+  else if (hand1 === 'paper') {
+    if (hand2 === 'rock') {
+      winningMessage = "Hand one wins!";
+    } else {
+      winningMessage = "Hand two wins!";
+    }
+  } // else if hand 1 = paper
+  else if (hand1 === 'scissors') {
+    if (hand2 === 'rock') {
+      winningMessage = "Hand two wins!";
+    } else {
+      winningMessage = "Hand one wins!";
+    }
+  }  // else if hand1 = scissors
+
+  return winningMessage;
+}  // rockPaperScissors
 
 function getPrompt() {
   rl.question('hand1: ', (answer1) => {
