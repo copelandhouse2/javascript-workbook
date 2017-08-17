be413abd734cb59d1a3ae03086c66b8d32686999
Craig Copeland <copelandhouse2@yahoo.com>
2017-08-16 20:15:15 -0500

    more TTT whiteboarding.  Initial code.

--- 06week/ticTacToe/script.js
+++ 06week/ticTacToe/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Your code here
+/***************  WHITE BOARD NOTES  ***************************
+define player turns: X, O
+On Click of a square... addEventListener, onclick
+  place playerTurn token into square.  append on the div data-cell
+  if (!checkForWin) then
+    if (!checkForTie) then
+      toggle playerTurn
+    else
+      message: There is a tie, you idiots;
+    end if
+  else
+    message: Congrats player???, you won!
+  end if
+
+  On Click of clear board button
+    remove tokens off of board
+    initialize any variables
+    set player to X
+
+***************************************************************/
 });
+
+playerTurn = playerTurn === 'X'? 'O' : 'X'
